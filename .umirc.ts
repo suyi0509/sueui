6f87b9bf9ce545b6162d741cb1023fb4d7588ff3
suyi <1124144330@qq.com>
2023-02-28 21:11:13 +0800

    feat: 初始化2.0

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: zhaoliang
  * @Date: 2023-02-28 20:52:07
- * @LastEditTime: 2023-02-28 20:52:07
+ * @LastEditTime: 2023-02-28 21:09:19
  * @LastEditors: zhaoliang
  * @Description: 如果代码不是为了制造bug，那将毫无意义
  * @FilePath: \test-ui\.umirc.ts
@@ -9,11 +9,21 @@
  */
 import { defineConfig } from 'dumi';
 
+let base = '/test-uui';
+let publicPath = '/test-uui/'
+
+if (process.env.SITE_BUILD_ENV === 'PREVIEW') {
+    base = undefined;
+    publicPath = undefined
+}
+
 export default defineConfig({
-  title: 'XIANZAO UI',
-  mode: 'site',
-  outputPath: 'doc-site',
- 	exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
-  dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
-  webpack5: {},
+    title: 'XIANZAO UI',
+    mode: 'site',
+    outputPath: 'doc-site',
+    exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面
+    dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
+    webpack5: {},
+    base,
+    publicPath,
 });
